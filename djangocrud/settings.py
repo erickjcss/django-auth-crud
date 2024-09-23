d84af85bb7a617a7eab0a61f9fbbb4fde6bcfb9c
erickjcss <erickmiralles363@gmail.com>
2024-09-23 15:16:27 -0400

    database

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -84,10 +84,7 @@ WSGI_APPLICATION = 'djangocrud.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-   """  'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    } """
+ 
         'default':dj_database_url.config(
             default='postgresql://postgres:postgres@localhost/postgres',
             conn_max_age=600
